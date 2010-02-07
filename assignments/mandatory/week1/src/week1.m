9c190817c6f71327f119be6916dd0683a47827e3
Ulrik Bonde <ulrikbonde@gmail.com>
2010-02-07 19:07:51 +0100

    Further work

--- assignments/mandatory/week1/src/week1.m
+++ assignments/mandatory/week1/src/week1.m
@@ -7,12 +7,10 @@ function week1( ~ )
         % Supplementary assignment 1.3
 
         % Rather use a confidence interval
-        % The mean would be acceptable also
+        % The mean would have been acceptable also
         confidence = [0.05; 0.95];
         img_min = quantile(quantile(I, confidence(1)), confidence(1));
         img_max = quantile(quantile(I, confidence(2)), confidence(2));
-        %img_min = 0;
-        %img_max = 120;
 
         % Target interval
         my_min = 0;
@@ -51,8 +49,9 @@ function week1( ~ )
                 bins(i, 2) = bins(i, 1) + int_width;
             end
         end
-        
-        % Rambo-fix
+                
+        % Rambo-fix for ensuring that max values are put in the
+        % last bin
         bins(n, 2) = bins(n, 2) + 1;
         
         % Get the new color values for the bins
@@ -61,7 +60,7 @@ function week1( ~ )
         % Put pixel values in bins, thus assigning new colors
         for x = (1:size(I,1))
             for y = (1:size(I,2))
-                for i = (1:n)
+                for i = 1:n
                     if bins(i,1) <= I(x,y) && I(x,y) < bins(i,2)
                         I(x,y) = vals(i);
                         break;
@@ -71,17 +70,154 @@ function week1( ~ )
         end
     end
 
-%filepath = '../../../images/cmp1.gif';
-filepath = '../../../images/idotyl.tiff';
+    function I = reduceVar(I, n)
+        % Uses bins from the imhist()-function
+        % but does the same thing as reduce( ... )
+        [count, bins] = imhist(I, n + 1);
+        bins(n + 1) = bins(n + 1) + 1;
+        
+        % Put pixel values in bins, thus assigning new colors
+        for m = 1:size(I,1)
+            for n = 1:size(I,2)
+                for i = 1:(length(bins) - 1)
+                    if bins(i) <= I(m,n) && I(m,n) < bins(i + 1)
+                        I(m,n) = floor((bins(i) + bins(i + 1))/2);
+                        break;
+                    end
+                end
+            end
+        end
+    end
 
-I = imread(filepath);
+    function I = factorResize(I, factor)
+        % Resize image with a 1/factor scale
+        % Each pixel is given the mean of the factor*factor
+        % neighbourhood
+        % -------------
+        % | x |   |   |
+        % |-----------|
+        % |   |   |   |    <- Mean on this given to x
+        % |-----------|
+        % |   |   |   |
+        % |-----------|
+        %
+        
+        [m, n] = size(I);
+        
+        % Calculate the extra width required for no modulo
+        extra_width = factor - mod(n, factor);
+        extra_height = factor - mod(m, factor);
+        
+        if (extra_width/factor) < 0.5 && mod(n, factor) ~= 0
+            tmp = mat2gray(zeros(m, n + extra_width));
+            tmp = im2uint8(tmp);
+            tmp(1:m, 1:n) = I(1:m,1:n);
+            
+            cop_col = I(1:m, n);
+            for i = 1:length(cop_col)
+                for j = n + 1:n + extra_width
+                    tmp(i, j) = cop_col(i);
+                end
+            end
+            
+            % Set the original image the modified
+            I = tmp;
+            
+            % Get rid of the extra image
+            clearvars tmp;
+        elseif extra_width ~= 0
+            % We want to clip the image
+            I = I(1:m , 1:n - mod(n, factor));
+        end
+        
+        [m, n] = size(I);
+        
+        if (extra_height/factor) < 0.5 && mod(m, factor) ~= 0
+            tmp = mat2gray(zeros(m + extra_height, n));
+            tmp = im2uint8(tmp);
+            tmp(1:m, 1:n) = I(:,:);
+            
+            cop_row = I(m, 1:n);
+            for i = m + 1:m + extra_height
+                for j = 1:length(cop_row)
+                    tmp(i, j) = cop_row(j);
+                end
+            end
+            I = tmp;
+            clearvars tmp;
+        elseif extra_height ~= 0
+            I = I(1:m - mod(m, factor), 1:n);
+        end
+        
+        [m, n] = size(I);
 
-I = adjust(I);
+        for x = 1:factor:n
+            for y = 1:factor:m
+                I(y:y + factor - 1, x:x + factor - 1) = floor(mean2(I(y:y + factor - 1, x:x + factor - 1)));
+            end
+        end
 
-n = 4;
-I = reduce(I, n);
-imshow(I);
-figure, imhist(I, n)
+    end
+
+    function I = quatersize(I, times)
+        % Supplementary excercise 1.4
+        for x = 1:times
+            I = I(1:2:size(I,1), 1:2:size(I,2));
+        end
+    end
+
+    function I = quadouble(I, times)
+        %Supplementary excercise 1.4
+        for i = (1:times)
+            M = zeros(size(I) * 2);
+            xoff = 0;
+            
+            for x = (1:size(I, 1))
+                yoff = 0;
+                for y = (1:size(I, 2))
+                    M(x + xoff, y + yoff) = I(x,y);
+                    M(x + 1 + xoff, y + yoff) = I(x,y);
+                    M(x + 1 + xoff, y + 1 + yoff) = I(x,y);
+                    M(x + xoff, y + 1 + yoff) = I(x,y);
+                    yoff = yoff + 1;
+                end
+                xoff = xoff + 1;
+            end
+            M = mat2gray(M);
+            M = im2uint8(M);
+            I = M;  
+        end
+    end
+
+    function I = resolution(I, scale)
+        kernel = fspecial('gaussian', [3 3], 5);
+        I = imfilter(I, kernel, 'symmetric', 'conv');
+        I = I(1:2:size(I,1), 1:2:size(I,2));
+    end
+        
+
+    function run( ~ )
+        filepath = '../../../../images/cmp1.gif';
+        %filepath = '../../../../images/idotyl.tiff';
+
+        I = imread(filepath);
+
+        I = adjust(I);
+
+        n = 8;
+        %R = reduce(I, n);
+        R = reduceVar(I, n);
+        
+        factor = 2;
+        M = factorResize(R, factor);
+        %M = I(1:factor:size(M,1), 1:factor:size(M,2));
+        
+        imshow(I), figure, imhist(I, 128);
+        figure, imshow(M), figure, imhist(M, n);
+        %figure, imshow(R);
+    end
+
+run();
 
 end
 
