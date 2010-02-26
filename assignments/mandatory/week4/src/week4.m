744bae44d574e5a72528804422c3d4163a34b32b
Ulrik Bonde <ulrikbonde@gmail.com>
2010-02-26 19:34:35 +0100

    More shit

--- assignments/mandatory/week4/src/week4.m
+++ assignments/mandatory/week4/src/week4.m
@@ -46,20 +46,20 @@ function week4( ~ )
        for p = 1:P - 1
            LP{p, 1} = GP{p, 1} - Gauss(GP{p, 1}, sigma);
        end
-       
+
        LP{P, 1} = GP{P, 1};
     end
 
     function LP = LaplacianPyramid(I, P, sigma)
         % Generate the Laplacian pyramid to a certain level
         GP = GaussianPyramid(I, P, sigma);
-        
+
         LP = cell(P, 1);
-        
+
         for p = 1:P - 1
             LP{p, 1} = GP{p, 1} - Upsample(GP{p + 1, 1});
         end
-        
+
         LP{P, 1} = GP{P, 1};
     end
 
@@ -68,7 +68,7 @@ function week4( ~ )
        F = fftshift(fft2(I));
        offM = size(I, 1)/2;
        offN = size(I, 2)/2;
-       
+
        G = zeros(size(I)*2);
        G(offM + 1:size(G,1) - offM, offN + 1:size(G,2) - offN) = F(:,:);
        G = abs(ifft2(G));
@@ -78,10 +78,19 @@ function week4( ~ )
     function G = Downsample(I)
        % We expect that the image have been blurred
        F = fftshift(fft2(I));
-       
-       offM = size(I,1)/4;
-       offN = size(I,2)/4;
-       
+
+       if size(I, 1) ~= 1
+           offM = size(I,1)/4;
+       else
+           offM = 0;
+       end
+
+       if size(I, 2) ~= 1
+           offN = size(I,2)/4;
+       else
+           offN = 0;
+       end
+
        F = F(offM + 1:size(I,1) - offM, offN + 1:size(I,2) - offN);
        G = abs(ifft2(F));
     end
@@ -91,10 +100,10 @@ function week4( ~ )
         % We do this by reconstructing the Gaussian Pyramid
         P = size(LP,1);
         GP = cell(P, 1);
-        
+
         % The top of the pyramids is equal
         GP{P, 1} = LP{P, 1};
-        
+
         for p = P:-1:2
             GP{p - 1, 1} = LP{p - 1, 1} + Upsample(GP{p, 1});
         end
@@ -103,9 +112,9 @@ function week4( ~ )
     function GP = GaussianPyramid(I, Q, sigma)
         % Construct a gaussian pyramid of height Q
         GP = cell(Q, 1);
-                
+
         GP{1, 1} = double(I);
-        
+
         for q = 1:Q - 1
             GP{q + 1, 1} = Downsample(Gauss(GP{q, 1}, sigma));
         end
@@ -115,10 +124,10 @@ function week4( ~ )
         % Compute the gaussian with specified sigma
         % Convolution is done in the frequency domain
         F = fft2(I);
-        
+
         h = fspecial('gaussian', size(I), sigma);
         h = fft2(h);
-                
+
         G = abs(fftshift(ifft2(h.*F)));
     end
 
@@ -139,58 +148,9 @@ function week4( ~ )
         %g1 = imread('../../../../images/square.tiff');
         %g1 = imread('../../../../images/unix.tiff');
         
-        imshow(Gauss(g1, 8) - Gauss(g1, 16), cmap);
-        %figure, imhist(g1);
-        
         sigma = 4;
         levels = 6;
-        
-        %LP1 = LaplacianPyramid(g1, levels, sigma);
-        %LP2 = AlternativeLaplacianPyramid(g1, levels, sigma);
-        
-        %ftshow(fftshift(fft2(g1)));
-        
-        %GP1 = ReconstructFromLaplacian(LP1);
-        %GP2 = ReconstructFromLaplacian(LP2);
-        
-        %ShowPyramid(GP1, cmap);
-        %ShowPyramid(LP2, cmap);
-        
-        %reconst1 = im2uint8(mat2gray(GP1{1,1}));
-        %reconst2 = im2uint8(mat2gray(GP2{1,1}));
-        
-        %reconst1 = adjust(reconst1);
-        %reconst2 = adjust(reconst2);
-        %g1 = adjust(g1);
-        
-        %diff1 = im2uint8(mat2gray(g1 - reconst1));
-        %diff2 = im2uint8(mat2gray(g1 - reconst2));
-        %for m = 1:size(g1,1)
-        %    for n = 1:size(g1,2)
-        %        if g1(m,n) ~= reconst1(m,n)
-                    %g1(m,n)
-                    %reconst1(m,n)
-        %            g1(m,n) = 255;
-        %        end
-        %    end
-        %end
-        
-        %figure, imshow(g1, []);
-        
-        %figure, imhist(reconst1);
-        %figure, imhist(reconst2);
-        %imwrite(g1, '../report/images/org.png', 'png');
-        %imwrite(reconst1, '../report/images/reconst1.png', 'png');
-        %imwrite(reconst2, '../report/images/reconst2.png', 'png');
-        
-        %ftshow(fftshift(fft2(reconst1)));
-        %ftshow(fftshift(fft2(reconst2)));
-        
-        %figure, imshow(reconst1, cmap);
-        %figure, imshow(reconst2, cmap);
-        
-        %figure, imshow(diff1, cmap);
-        %figure, imshow(diff2, cmap);
+        % Write something
     end
 
 % PART 2
@@ -199,44 +159,47 @@ function week4( ~ )
         N = 64;
         S = [zeros(1,20),ones(1,3),zeros(1,2),ones(1,5)];
         S = [S,zeros(1,N-length(S))] + 0.1*randn(1,N);
-        plot(S)
+    end
+
+    function I = ScalesOfSignal(S, levels)
+        I = zeros(levels, length(S));
+        I(1,:) = S;
+        for sigma = 1:levels - 1
+            %I(sigma + 1, :) = Gauss(S, sigma);
+            I(sigma + 1, :) = Gauss(I(sigma,:), sigma);
+        end
+    end
+
+    function I = AltScalesOfSignal(S, levels)
+        I = zeros(levels, length(S));
+        I(1,:) = S;
+        for sigma = 1:levels - 1
+            I(sigma + 1, :) = Gauss(S, sigma);
+        end
+    end
+
+    function peaks = FindMaxPyr(PYR)
+        peaks = zeros(length(PYR), 2);
+        for i = 1:length(PYR)
+            [peaks(i, 1) peaks(i, 2)] = max(PYR{i, 1});
+        end
     end
 
     function part2( ~ )
         S = ConstructSignal();
-        
-        P = zeros(length(S), length(S));
-        for i = 1:size(P,1)
-            P(i,:) = S;
-        end
-        
-        %figure, imshow(S, []);
-        
-        F = fftshift(fft2(S));
-        
-        figure, plot(Gauss(S, 0.2));
+        S = adjust(S);
+        s1 = ScalesOfSignal(S, length(S));
+        s2 = AltScalesOfSignal(S, length(S));
+        figure, imshow(s1, []);
+        figure, imshow(s2, []);
 
-        %s = 0.20;
-        %n = length(S);
-        %x = -1/2:1/(n-1):1/2;
-        %h = exp( -(x.^2)/(2*s^2));
-        %h = h / sum(sum(h));
-        
-        figure, plot(log(abs(F)));
-        %figure, plot(log(abs(h.*F)));
-
-        %figure, plot(h);
-        
-        %S = abs(ifft(h.*F));
-        %figure, plot(S);
-
-    end
-
-    function run( ~ )
-        part1;
+        %figure('Position', [1 1 600 800]);
+        %for i = 1:6
+        %    subplot(3, 2, i), plot(s(i, :));
+        %end
     end
 
 close all;
-run();
+part2();
 
 end
