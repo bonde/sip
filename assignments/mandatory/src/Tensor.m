e43fbf46ef5b2bf5e3ed48fb4f27c4e52f381363
Ulrik Bonde <ulrikbonde@gmail.com>
2010-03-18 11:28:44 +0100

    IGalol

--- assignments/mandatory/src/Tensor.m
+++ assignments/mandatory/src/Tensor.m
@@ -1,36 +1,26 @@
-function [u v] = Tensor(I, scale, varargin)
+function [u v] = Tensor(I, scale, sigma1, sigma2)
 
+% Use external package from http://staff.science.uva.nl/~rein/nldiffusionweb/material.html
 addpath('../diffusion/');
 
-if nargin == 2
-    sigma1 = 5;
-    sigma2 = 4;
-elseif nargin == 3
-    sigma1 = varargin(1);
-    sigma1 = sigma1{1};
-    sigma2 = 4;
-elseif nargin == 4
-    sigma1 = varargin(1);
-    sigma1 = sigma1{1};
-    sigma2 = varargin(2);
-    sigma2 = sigma2{1};
-end
-
+% Find the derivatives on the scale defined by sigma1
 Dxx = gD(I, sigma1, 2, 0);
 Dyy = gD(I, sigma1, 0, 2);
 Dxy = gD(I, sigma1, 1, 1);
 
+% Convolve the derivatives with a gaussian
 a = gD(Dxx, sigma2, 0, 0);
 b = gD(Dxy, sigma2, 0, 0);
 c = gD(Dyy, sigma2, 0, 0);
 
+% Get vectors u and v
 u = 2*b./(c - a + sqrt(c.^2 - 2*a.*c + a.^2+4*b.^2));
 v = 2*b./(-c + a + sqrt(c.^2 - 2*a.*c + a.^2+4*b.^2));
 
+% Normalize u and v (according to scale)
 normal = ones(size(u));
 lu = sqrt(u.^2 + normal.^2);
 lv = sqrt(v.^2 + normal.^2);
-
 u = 0.75*scale*u./lu;
 v = 0.75*scale*v./lv;
 
--- assignments/mandatory/src/assignment621.m
+++ assignments/mandatory/src/assignment621.m
@@ -2,30 +2,62 @@ function assignment621( ~ )
 
 addpath('../diffusion/');
 
+tensor = 0;
+grad = 1;
+
 close all;
 
-sigma1 = 3;
+sigma1 = 6;
 sigma2 = 4;
-step = 4;
+step = 6;
 
 [I cmap] = imread('../../../images/FINGERPRINT.jpg');
 %I = imread('../../../images/lenna.tiff');
 
 I = double(I);
-[u v] = Tensor(I, step, sigma1, sigma2);
+if tensor
+    [u v] = Tensor(I, step, sigma1, sigma2);
+end
+
+if grad
+    p = gD(I, sigma1, 1, 0);
+    q = gD(I, sigma1, 0, 1);
+
+    % Normalize u and v (according to scale)
+    normal = ones(size(p));
+    lp = sqrt(p.^2 + normal.^2);
+    lq = sqrt(q.^2 + normal.^2);
+    u = 0.75*step*p./lp;
+    v = 0.75*step*q./lq;
+end
 
 I = gD(I, sigma1, 0, 0);
 
-[x y] = meshgrid(linspace(1, size(u,1), size(u,1)/step));
+%if grad
+%    [p q] = gradient(I);
+%end
 
-u = interp2(u, x, y);
-v = interp2(v, x, y);
+[x y] = meshgrid(linspace(1, size(I,1), size(I,1)/step));
+
+if tensor
+    u = interp2(u, x, y);
+    v = interp2(v, x, y);
+end
+if grad
+    p = interp2(p, x, y);
+    q = interp2(q, x, y);
+end
 
 figure(1);
 imshow(I, []);
 hold on;
-contour(I, [100 100], 'r');
-quiver(x-.5*v, y-.5*u, v, u);
+contour(I, [110 110], 'r');
+if tensor
+    quiver(x-.5*v, y-.5*u, v, u);
+end
+if grad
+    quiver(x-.5*p, y-.5*q, p, q, 'g');
+end
 hold off;
 
 end
